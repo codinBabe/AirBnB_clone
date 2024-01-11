985992a8888c086c53ec33d17a22a3cc4ce1cd7b
codinBabe <teboola2@gmail.com>
2024-01-11 22:03:58 +0100

    Create BaseModel from dictionary

--- tests/test_models/base_model.py
+++ tests/test_models/base_model.py
@@ -5,10 +5,19 @@ from datetime import datetime
 
 class BaseModel:
     """class that defines all common atrribtes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            del kwargs["__class__"]
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    strp_dtime = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, strp_dtime)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime"""
